63284d9981a13dbcd0a0aaa9e258d2d984b0bec5
Hugo Oliveira <hugo684@gmail.com>
2022-01-05 21:59:18 -0300

    first commit

---
+++ lib/rockelivery_web/controllers/user_controller.ex
@@ -0,0 +1,13 @@
+defmodule RockeliveryWeb.UserController do
+  use RockeliveryWeb, :controller
+
+  alias Rockelivery.User
+
+  def create(connection, params) do
+    with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
+      connection
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
+  end
+end
